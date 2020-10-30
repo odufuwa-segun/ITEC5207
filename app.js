a540122add25127cc2cc1e6690f6919f86c6b08d
Odufuwa Segun <odufuwas@gmail.com>
2020-10-30 01:19:23 -0400

    CSV Export
    Added support for exporting to csv

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 var express = require("express");
+const jsonexport = require('jsonexport');
 var app = express();
 var db = require("./database.js");
 var initialize_db = require("./initialize_db.js");
 
 var port = process.env.PORT || 3000;
 
+var json2csv2stdout = (res, jsondata, filename) => {
+    jsonexport(jsondata, function(err, csv){
+        if (err) {
+            res.status(400).message('Error occurred while processing output. '+ err);
+            return console.error(err);
+        }
+        res.header('Content-Type', 'text/csv');
+        res.attachment(filename);
+        return res.send(csv);
+        console.log(csv);
+    });
+};
 
 // Root endpoint
 app.get("/", (req, res, next) => {
     res.json({"message":"Welcome to ITEC5207 Backend"})
 });
-// app.get("/init", (req, res, next) => {
-//     initialize_db(db);
-//     res.json({"message":"Ok"})
-// });
+app.get("/api/init", (req, res, next) => {
+    initialize_db(db);
+    res.json({"message":"Ok"})
+});
 app.get("/api/publications", (req, res, next) => {
     // return all publications
     var sql = "select * from publication WHERE 1=1 "
@@ -48,6 +61,10 @@ app.get("/api/publications", (req, res, next) => {
           res.status(400).json({"error":err.message});
           return;
         }
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, rows, 'publications.csv');
+        }
         res.json({
             "message":"success",
             "data":rows
@@ -63,6 +80,10 @@ app.get("/api/publication/:id", (req, res, next) => {
           res.status(400).json({"error":err.message});
           return;
         }
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, row, 'publications-'+req.params.id+'.csv');
+        }
         res.json({
             "message":"success",
             "data":row
@@ -72,28 +93,73 @@ app.get("/api/publication/:id", (req, res, next) => {
 });
 app.get("/api/categories", (req, res, next) => {
     // return all categories
-    var sql = "select category, sum(males) as males, sum(females) as females, count(id) as publications from publication group by category";
+    var sql = "select categories, males, females from publication";
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
           res.status(400).json({"error":err.message});
           return;
         }
+        console.log(rows)
+        var categories = [];
+        // console.log('Start Aggregation')
+        var count=1;
+        rows.forEach(row => { 
+            var raw_categories = row.categories.split('|');
+            var males = row.males;
+            var females = row.females;
+            // console.log('Aggregating: '+count)
+            // console.log(raw_categories)
+            count++;
+            raw_categories.forEach(raw_category => {
+                if (isNaN(raw_category)) {
+                    // console.log(raw_category);
+                    var category = {
+                        name: raw_category,
+                        males: males,
+                        females: females,
+                        publications: 1
+                    };
+                    if (categories[raw_category] === undefined){
+                        // make sure its not already in the array
+                        categories[raw_category] = category;
+                    } else {
+                        categories[raw_category].males = categories[raw_category].males+males;
+                        categories[raw_category].females = categories[raw_category].females+females;
+                        categories[raw_category].publications++;
+                    }
+                }
+                
+            });
+        }); 
+        categories = Object.keys(categories).map(val => categories[val]);
+        // console.log('Done Aggregation')
+        // console.log(categories);
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, categories, 'categories.csv');
+        }
         res.json({
             "message":"success",
-            "data":rows
+            "data":categories
         })
     });
 });
 app.get("/api/category/:category", (req, res, next) => {
     // return all categories
-    var sql = "select category, sum(males) as males, sum(females) as females, count(id) as publications from publication where  category = ? group by category";
-    var params = [req.params.category]
+    var sql = "select sum(males) as males, sum(females) as females, count(id) as publications from publication where categories LIKE ?";
+    var params = ['%'+req.params.category+'%']
     db.get(sql, params, (err, rows) => {
         if (err) {
           res.status(400).json({"error":err.message});
           return;
         }
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, categories, 'category-'+req.params.category+'.csv');
+        }
+        // console.log(rows)
+        rows['category'] = req.params.category;
         res.json({
             "message":"success",
             "data":rows
@@ -102,8 +168,8 @@ app.get("/api/category/:category", (req, res, next) => {
 });
 app.get("/api/category/:category/publications", (req, res, next) => {
     // return all categories
-    var sql = "select * from publication where category = ?";
-    var params = [req.params.category]
+    var sql = "select * from publication where categories LIKE ?";
+    var params = ['%'+req.params.category+'%']
 
     let submitted_after = req.query.submitted_after; // format=YYYY-MM-DD
     let submitted_before = req.query.submitted_before;
@@ -131,6 +197,29 @@ app.get("/api/category/:category/publications", (req, res, next) => {
         if (err) {
           res.status(400).json({"error":err.message});
           return;
+        }
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, rows, 'category-'+req.params.category+'.publications.csv');
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
+app.get("/api/journals", (req, res, next) => {
+    // return all journals
+    var sql = "select distinct journal, sum(males) as males, sum(females) as females, count(*) as publications from publication where journal !='' GROUP By journal ";
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        if (req.query.format === 'csv'){
+            // console.log(rows);
+            return json2csv2stdout(res, rows, 'journals.csv');
         }
         res.json({
             "message":"success",
--- initialize_db.js
+++ initialize_db.js
@@ -2,23 +2,28 @@ var sqlite3 = require('sqlite3').verbose()
 var csv = require('csv-parser');
 var fs = require('fs');
 
+var csv_file = 'db/cleaned.csv';
+
 let create_sql = `CREATE TABLE publication(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        publication_id text,
-        submitted text,
-        updated	text,
-        title text,
-        abstract text,
-        authors	text,
-        link_abstract text,
-        link_pdf text,
-        link_doi text,
-        comment text, 
-        journal text,
-        category text, 
-        males INTEGER,
-        females INTEGER
-    )`;
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    publication_id text,
+    submitted text,
+    updated	text,
+    title text,
+    authors	text,
+    affiliations text,
+    link_abstract text,
+    link_pdf text,
+    link_doi text,
+    comment text, 
+    journal text,
+    primary_category text, 
+    categories text, 
+    males INTEGER,
+    females INTEGER
+)`;
+
+
 let initialize  = (db) => {
     db.run(create_sql, (err) => {
         console.log('Initializing Database.');
@@ -31,20 +36,56 @@ let initialize  = (db) => {
 };
 let insert_sql = '';
 var insert = `INSERT INTO publication (
-    publication_id, submitted, updated, title, abstract, authors, link_abstract, link_pdf, link_doi, comment, journal, category, males, females) 
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+    publication_id, submitted, updated, title, authors, affiliations, link_abstract, link_pdf, link_doi, comment, journal, primary_category, categories, males, females) 
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
 let data_csv_2_db = (db) => {
     console.log('Importing CSV data to SQLLite. ');
-    fs.createReadStream('db/cleaned.csv')
+    var count=0;
+    fs.createReadStream(csv_file)
         .pipe(csv())
         .on('data', (row) => {
-            // publication_id, submitted, updated, title, abstract, authors, link_abstract, link_pdf, link_doi, comment, journal, category, males, females
-            console.log(row.id);
-            db.run(insert, [row.id, row.submitted, row.updated, row.title, row.abstract, row.authors, row.link_abstract, row.link_pdf, row.link_doi, 
-                row.comment, row.journal, row.category, row.males, row.females]);
+            // publication_id, submitted, updated, title, authors, affiliations, link_abstract, link_pdf, link_doi, comment, journal, primary_category, categories, males, females
+            
+            var raw_authors = row.authors.split('|');
+            var authors = [];
+            raw_authors.forEach(raw_author => { 
+                raw_author = raw_author.trim();
+                if (authors.indexOf(raw_author)===-1){
+                    // make sure its not already in the array
+                    authors.push(raw_author);
+                }
+            }); 
+            authors = authors.join('|');
+            // var raw_categories = row.categories.replace(/,/gi,'|').split('|');
+            var raw_categories = row.categories.split('|');
+            var categories = [];
+            raw_categories.forEach(raw_category => { 
+                raw_category = raw_category.trim();
+                if (categories.indexOf(raw_category)===-1){
+                    // make sure its not already in the array
+                    categories.push(raw_category);
+                }
+            }); 
+            categories = categories.join('|');
+            var raw_affiliations = row.affiliations.split('|');
+            var affiliations = [];
+            raw_affiliations.forEach(raw_affiliation => { 
+                raw_affiliation = raw_affiliation.trim();
+                if (affiliations.indexOf(raw_affiliation)===-1){
+                    // make sure its not already in the array
+                    affiliations.push(raw_affiliation);
+                }
+            }); 
+            affiliations = affiliations.join('|');
+            
+            console.log(row.id+': '+ authors);
+
+            db.run(insert, [row.id, row.submitted, row.updated, row.title.trim(), authors, affiliations, row.link_abstract, row.link_pdf, row.link_doi, 
+                row.comment.trim(), row.journal.trim(), row.primary_category.trim(), categories, row.males, row.females]);
+                count++;
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('CSV file successfully processed: '+ count);
         });
 
 };
